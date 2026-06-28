4256134fbaf52aca0167adcd66f6b35882aee609
Aryan Pawar <aryanzeal22105@gmail.com>
2026-06-28 21:35:14 +0530

    fix: add hardhat-chai-matchers import to contract tests

--- packages/contracts/test/SkillPassportNFT.test.ts
+++ packages/contracts/test/SkillPassportNFT.test.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import "@nomicfoundation/hardhat-chai-matchers";
 import type { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 describe("SkillPassportNFT", function () {
